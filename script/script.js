eacc1d80d94cbbf732b0e4d82be301ea08d154b1
Junior_Kamanda <jkamanda062gmail.com>
2024-12-24 18:26:53 +0100

    yoga_app commit deux

--- script/script.js
+++ script/script.js
@@ -2,6 +2,19 @@ const h1 = document.querySelector("h1");
 const main = document.querySelector("main");
 const btnContainer = document.querySelector(".btnContainer");
 
+let exercicesArray = [
+  { pic: 0, min: 1 },
+  { pic: 1, min: 1 },
+  { pic: 2, min: 1 },
+  { pic: 3, min: 1 },
+  { pic: 4, min: 1 },
+  { pic: 5, min: 1 },
+  { pic: 6, min: 1 },
+  { pic: 7, min: 1 },
+  { pic: 8, min: 1 },
+  { pic: 9, min: 1 },
+];
+
 const utils = {
   pageContent: function (title, content, btn) {
     h1.innerHTML = title;
@@ -12,9 +25,23 @@ const utils = {
 
 const pages = {
   lobby: function () {
+    let mapArray = exercicesArray.map((exo) => {
+      return `
+        <li>
+          <div class="card-header">
+          <input type="number" min="1" max="10" value="${exo.min}" id=${exo.pic}>
+          <span>min</span>
+          </div>
+          <img src="../img/${exo.pic}.png" alt="exercice ${exo.pic}">
+          <i class="fas fa-arrow-alt-circle-left arrow"></i>
+          <i class="fas fa-times-circle deleteBtn"></i>
+        </li>
+      `;
+    }).join("");
+
     utils.pageContent(
-      `Paramétrage <i id="reboot" class="fa fa-cog"></i>`,
-      `Exercice`,
+      `Paramétrage <i id="reboot" class="fas fa-undo"></i>`,
+      `<ul>${mapArray}</ul>`,
       `<button id="start">Démarrer<i class="fa fa-play"></i></button>`
     );
   },
